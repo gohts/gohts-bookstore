d82d4e7c36b1b9212af3d6fe7ab3c8b622e72262
Tiong Sheng Goh <gohts@sg.ibm.com>
2020-11-06 11:22:37 +0800

    removed mysql details

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ const pool = mysql.createPool({
     port: parseInt(process.env.DB_PORT) || 3306,
     database: process.env.DB_NAME || 'goodreads',
     // review again
-    user: process.env.DB_USER || 'wilma',
-    password: process.env.DB_PASSWORD || 'wilma',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     connectionLimit: 4,
     timezone: '+08:00'
 })
